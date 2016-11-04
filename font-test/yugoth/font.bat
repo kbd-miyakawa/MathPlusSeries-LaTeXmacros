2f41c4172166ce8b0a85b9c51601ac1c686a5b0b
kbd-miyakawa <densan@keibundou.co.jp>
2016-11-04 20:01:15 +0900

    ノンブル書体変更，章見出し体裁変更

---
+++ font-test/yugoth/font.bat
@@ -0,0 +1,10 @@
+UniteTTC YUGOTHM.TTC
+del YUGOTHM002.ttf
+otftotfm --no-type1 -e 7t -fkern -fliga YUGOTHM001.TTF -n yugothm 
+goto next
+UniteTTC YUGOTHL.TTC
+del YUGOTHL002.ttf
+rem otftotfm --no-type1 -e 7t -fkern -fliga YUGOTHL001 -n yugothl >> yugoth.map
+
+
+:next
